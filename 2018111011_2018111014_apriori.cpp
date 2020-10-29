8e99a82eb89d0a08bcceaebc140dc12e7facfe33
codernik14 <nikunjnawal14@gmail.com>
2020-10-29 23:50:58 +0530

    Final Submission

---
+++ 2018111011_2018111014_apriori.cpp
@@ -0,0 +1,526 @@
+#include<bits/stdc++.h>
+using namespace std;
+using namespace std::chrono;
+#define pb push_back
+#define mp make_pair
+typedef long long int ll;
+typedef string str;
+
+bool has_infrequent_subset(vector <ll> c,set <vector<ll> > lk)
+{
+	for(ll i=0;i<c.size();i++)
+	{
+		vector <ll> tem(c.begin(),c.end());
+		tem.erase(tem.begin()+i);
+		if(lk.find(tem)==lk.end())
+			return false;
+	}
+	return true;
+}
+
+vector <vector<ll> > apriori_gen(vector <vector<ll> > lk,ll k)
+{
+	ll i,n=lk.size();
+	vector <vector <ll> > ck;
+	set <vector<ll> > lk1;
+	for(auto it:lk)
+	{
+		// sort(it.begin(),it.end());
+		lk1.insert(it);
+	}
+	for(ll l=0;l<n;l++)
+	{
+		for(ll m=l+1;m<n;m++)
+		{
+			for(i=0;i<k;i++)
+			{
+				if(lk[l][i]!=lk[m][i])
+				{
+					break;
+				}
+			}
+			if(i==k-1)
+			{
+				vector <ll> v(lk[l].begin(), lk[l].end());
+				v.pb(lk[m][k-1]);
+				if(has_infrequent_subset(v,lk1))
+				{
+					ck.pb(v);
+				}
+			}
+		}
+	}
+	return ck; 	
+}
+
+
+
+vector <vector <vector <ll> > > apriori(vector <set<ll> > v, ll sup)
+{
+	cout << "\n--------------------NORMAL APRIORI ONLY--------------------\n\n";
+	auto start = high_resolution_clock::now();
+	
+	ll k=1;
+	vector <vector <vector <ll> > > ans;
+	vector <vector <ll> > lk,ck;
+	// For frequent itemsets of size 1 using hash map
+	map <ll ,ll> m0;
+	for(ll i=0;i<v.size();i++)
+	{
+		for(auto j:v[i])
+			m0[j]++;
+	}
+	for(auto it:m0)
+	{
+		if(it.second >= sup)
+		{
+			vector <ll> p; 
+			p.pb(it.first);
+			lk.pb(p);
+		}
+	}
+    // For frequent itemsets of size greater than 1
+	while(lk.size()!=0)
+	{
+		ans.pb(lk);
+		ck=apriori_gen(lk,k);
+		lk.clear();
+		k++;
+		ll n=ck.size();
+		ll cnt[n+2]={0};
+		for(auto s:v)
+		{
+			if(k>s.size())
+				continue;
+			for(ll l=0;l<n;l++)
+			{
+				ll o=ck[l].size(),m;
+				for(m=0;m<o;m++)
+				{
+					if(s.find(ck[l][m])==s.end())
+					{
+						break;
+					}
+				}
+				if(m==o)
+					cnt[l]++;
+			}
+		}
+		for(ll l=0;l<n;l++)
+		{
+			if(cnt[l] >= sup)
+			{
+				lk.pb(ck[l]);
+			}
+		}
+		
+	}
+	
+	auto stop = high_resolution_clock::now(); 
+	auto duration = duration_cast<microseconds>(stop - start); 
+	cout << "TIME TAKEN BY ALGORITHM IS "<< float(duration.count()/1000000.0) << " seconds" << endl;
+	
+	return ans;
+}
+
+
+
+vector <vector <vector <ll> > > hashed_apriori(vector <set<ll> > v, ll sup)
+{
+	cout << "\n--------------------HASHED ONLY--------------------\n\n";
+	auto start = high_resolution_clock::now();
+	ll k=1;
+	vector <vector <vector <ll> > > ans;
+	vector <vector <ll> > lk,ck;
+
+	// For frequent itemsets of size 1 and size 2 using hash map
+	map <ll ,ll> m0;
+	map <pair<ll,ll> , ll> m1;	
+	for(ll i=0;i<v.size();i++)
+	{
+		for(auto it=v[i].begin();it!=v[i].end();it++)
+		{
+			it++;
+			auto it1=it;
+			it--;
+			for(;it1!=v[i].end();it1++)
+			{
+				m1[mp(*it,*it1)]++;
+			}
+			m0[*it]++;
+		}
+	}
+
+	for(auto it:m0)
+	{
+		if(it.second >= sup)
+		{
+			vector <ll> p; 
+			p.pb(it.first);
+			lk.pb(p);
+		}
+	}
+	if(lk.size()==0)
+	{
+		auto stop = high_resolution_clock::now(); 
+		auto duration = duration_cast<microseconds>(stop - start); 
+		cout << "TIME TAKEN BY ALGORITHM IS "<< float(duration.count()/1000000.0) << " seconds" << endl;
+		return ans;	
+    }
+    ans.pb(lk);
+    k++;
+    lk.clear();
+	
+
+    for(auto it:m1)
+    {
+    	if(it.second >= sup)
+    	{
+    		vector <ll> p; 
+   			p.pb(it.first.first);
+   			p.pb(it.first.second);
+        	lk.pb(p); 
+    	}
+    }
+
+    // For frequent itemsets of size greater than 1
+	while(lk.size()!=0)
+	{
+		ans.pb(lk);
+		ck=apriori_gen(lk,k);
+		lk.clear();
+		k++;
+		ll n=ck.size();
+		ll cnt[n+2]={0};
+		for(auto s:v)
+		{
+			if(k>s.size())
+				continue;
+			for(ll l=0;l<n;l++)
+			{
+				ll o=ck[l].size(),m;
+				for(m=0;m<o;m++)
+				{
+					if(s.find(ck[l][m])==s.end())
+					{
+						break;
+					}
+				}
+				if(m==o)
+					cnt[l]++;
+			}
+		}
+		for(ll l=0;l<n;l++)
+		{
+			if(cnt[l] >= sup)
+			{
+				lk.pb(ck[l]);
+			}
+		}
+		
+	}
+	auto stop = high_resolution_clock::now(); 
+	auto duration = duration_cast<microseconds>(stop - start); 
+	cout << "TIME TAKEN BY ALGORITHM IS "<< float(duration.count()/1000000.0) << " seconds" << endl;
+	
+	return ans;
+}
+
+
+
+vector <vector <vector <ll> > > transaction_reduction(vector <set<ll> > v, ll sup)
+{
+	cout << "\n--------------------TRANSACTION REDUCTION ONLY--------------------\n\n";
+	auto start = high_resolution_clock::now();
+	ll k=1;
+	vector <vector <vector <ll> > > ans;
+	vector <vector <ll> > lk,ck;
+	vector <set<ll> > v1,v2;
+	// For frequent itemsets of size 1 using hash map
+	map <ll ,ll> m0;
+	for(ll i=0;i<v.size();i++)
+	{
+		for(auto j:v[i])
+			m0[j]++;
+		if(v[i].size()>1)
+		{
+			v2.pb(v[i]);
+		}
+	}
+	for(auto it:m0)
+	{
+		if(it.second >= sup)
+		{
+			vector <ll> p; 
+			p.pb(it.first);
+			lk.pb(p);
+		}
+	}
+    vector <ll> temp;
+    // For frequent itemsets of size greater than 1
+	while(lk.size()!=0 && v2.size())
+	{
+		ans.pb(lk);
+		ck=apriori_gen(lk,k);
+		lk.clear();
+		k++;
+		v1.clear();
+		for(auto s:v2)
+		{
+			v1.pb(s);
+		}
+		v2.clear();
+		ll n=ck.size();
+		ll cnt[n+2]={0};
+		if(v1.size()!=0)
+		{
+			for(auto s:v1)
+			{
+				ll flag=0;
+				for(ll l=0;l<n;l++)
+				{
+					ll o=ck[l].size(),m;
+					for(m=0;m<o;m++)
+					{
+						if(s.find(ck[l][m])==s.end())
+						{
+							break;
+						}
+					}
+					if(m==o)
+					{	
+						cnt[l]++;
+						flag=1;	
+					}
+				}
+				if(flag && s.size()>k)
+					v2.pb(s);
+			}
+			
+		}
+		for(ll l=0;l<n;l++)
+		{
+			if(cnt[l] >= sup)
+			{
+				lk.pb(ck[l]);
+			}
+		}
+	}
+	auto stop = high_resolution_clock::now(); 
+	auto duration = duration_cast<microseconds>(stop - start); 
+	cout << "TIME TAKEN BY ALGORITHM IS "<< float(duration.count()/1000000.0) << " seconds" << endl;
+	return ans;
+}
+
+
+
+vector <vector <vector <ll> > > reduction_hashed_apriori(vector <set<ll> > v, ll sup)
+{
+	cout << "\n--------------------BOTH HASHING AND TRANSACTION REDUCTION COMBINED--------------------\n\n";
+	auto start = high_resolution_clock::now();
+	ll k=1;
+	vector <vector <vector <ll> > > ans;
+	vector <vector <ll> > lk,ck;
+
+	// For frequent itemsets of size 1 and size 2 using hash map
+	map <ll ,ll> m0;
+	map <pair<ll,ll> , ll> m1;	
+	vector < set<ll> > v1,v2; 
+	for(ll i=0;i<v.size();i++)
+	{
+		for(auto it=v[i].begin();it!=v[i].end();it++)
+		{
+			it++;
+			auto it1=it;
+			it--;
+			for(;it1!=v[i].end();it1++)
+			{
+				m1[mp(*it,*it1)]++;
+			}
+			m0[*it]++;
+		}
+		if(v[i].size()>2)
+		{
+			v2.pb(v[i]);
+		}
+	}
+
+	for(auto it:m0)
+	{
+		if(it.second >= sup)
+		{
+			vector <ll> p; 
+			p.pb(it.first);
+			lk.pb(p);
+		}
+	}
+	if(lk.size()==0)
+	{
+		auto stop = high_resolution_clock::now(); 
+		auto duration = duration_cast<microseconds>(stop - start); 
+		cout << "TIME TAKEN BY ALGORITHM IS "<< float(duration.count()/1000000.0) << " seconds" << endl;
+		return ans;	
+    }
+    ans.pb(lk);
+    k++;
+    lk.clear();
+	
+
+    for(auto it:m1)
+    {
+    	if(it.second >= sup)
+    	{
+    		vector <ll> p; 
+   			p.pb(it.first.first);
+   			p.pb(it.first.second);
+        	lk.pb(p); 
+    	}
+    }
+
+
+    // For frequent itemsets of size greater than 2
+	vector <ll> temp;    
+	while(lk.size()!=0 && v2.size())
+	{
+		ans.pb(lk);
+		ck=apriori_gen(lk,k);
+		lk.clear();
+		k++;
+		v1.clear();
+		for(auto s:v2)
+		{
+			v1.pb(s);
+		}
+		v2.clear();
+		ll n=ck.size();
+		ll cnt[n+2]={0};
+		if(v1.size()!=0)
+		{
+			for(auto s:v1)
+			{
+				ll flag=0;
+				for(ll l=0;l<n;l++)
+				{
+					ll o=ck[l].size(),m;
+					for(m=0;m<o;m++)
+					{
+						if(s.find(ck[l][m])==s.end())
+						{
+							break;
+						}
+					}
+					if(m==o)
+					{	
+						cnt[l]++;
+						flag=1;	
+					}
+				}
+				if(flag && s.size()>k)
+					v2.pb(s);
+			}
+			
+		}
+		for(ll l=0;l<n;l++)
+		{
+			if(cnt[l] >= sup)
+			{
+				lk.pb(ck[l]);
+			}
+		}
+	}
+	auto stop = high_resolution_clock::now(); 
+	auto duration = duration_cast<microseconds>(stop - start); 
+	cout << "TIME TAKEN BY ALGORITHM IS "<< float(duration.count()/1000000.0) << " seconds" << endl;
+	
+	return ans;
+}
+
+
+
+void show_results(vector <vector <vector <ll> > > ans)
+{
+	ll z=0,j,k;
+	if(ans.size()!=0)
+    for(auto i : ans)
+    {
+    	cout << "\nNUMBER OF FREQUENT ITEMSETS OF SIZE=" << ++z << " ARE " << i.size() << " WHICH ARE-\n\n[";
+        for(j=0;j<i.size()-1;j++)
+        {
+        	cout << "[";
+        	for(k=0;k<i[j].size()-1;k++)
+        	{
+        		cout << i[j][k] << ",";
+        	}
+        	cout << i[j][k] << "],";
+        }
+        cout << "[";
+        for(k=0;k<i[j].size()-1;k++)
+        {
+        	cout << i[j][k] << ",";
+        }
+        cout << i[j][k] << "]";
+        cout << "]\n";
+    }
+    else
+    {
+    	cout << "\nNO FREQUENT ITEMSETS\n";
+    }
+    cout << "\n\n";
+}
+
+
+
+
+int main()
+{
+	str file_path,line;
+	cout << "Input The Path To The File -> ";
+	cin >> file_path;
+
+	ifstream fin(file_path, ios::in);
+    ll i = 0;
+    vector<set<ll>> v;
+    while(getline(fin, line))
+    {
+        stringstream s(line);
+        string word;
+        set<ll> t;
+        while(getline(s,word,' '))
+        {
+            ll temp = stoi(word);
+            if(temp > 0)
+            {
+                t.insert(temp);
+            }
+        }
+        v.pb(t);
+        i++;
+    }
+    double min_sup,n;
+    ll sup;
+    cout << "Input The Minimum Support Count (in percentage) -> ";
+    cin >> min_sup;
+    n=(double)v.size();
+    n*=min_sup;
+    n/=100;
+    sup=n;
+    cout << "Minimum Support Count Is " << sup << "\n";
+    vector <vector <vector <ll> > > ans,ans1,ans2,ans3;
+
+    // For Normal Apriori Algorithm
+    ans=apriori(v,sup);
+    show_results(ans);
+
+    //For Hashed Apriori Algorithm
+    ans1=hashed_apriori(v,sup);
+    show_results(ans1);
+
+    //For Transaction Reduction In Apriori Algorithm
+    ans2=transaction_reduction(v,sup);
+    show_results(ans2);
+
+    //For Both Transaction Reduction and Hashing Together In Apriori
+    ans3=reduction_hashed_apriori(v,sup);
+    show_results(ans3);
+    return 0;
+}
---
+++ 2018111011_2018111014_fpg.cpp
@@ -0,0 +1,600 @@
+#include<bits/stdc++.h>
+#include<fstream>
+using namespace std;
+using namespace std::chrono;
+#define int long long
+
+struct node
+{
+    unordered_map<int,node*> pointer;
+    int freq;
+    int item;
+    node* parent;
+};
+
+
+vector<vector<int>> finalans;
+string get_binary(int n)
+{
+    string ret = "";
+    while(n > 0)
+    {
+        int t = n%2;
+        if(t == 0)
+            ret += '0';
+        else
+            ret += '1';
+        n = n/2;
+    }
+    return ret;
+}
+  
+void print_Tree(node *cur)
+{
+    for(auto i: cur->pointer)
+    {
+        //cout <<cur->item<<" "<<i.second->item<<" "<<i.second->freq<<endl;
+        print_Tree(i.second);
+    }
+
+}
+void FP_Mine(vector<pair<int,node*>> backtrack,vector<int> v,vector<int> alpha,int min_sup);
+void FP_Growth(vector<set<int>> transactions, int min_sup, vector<int> alpha);
+
+void FP_Mine_Merge(vector<pair<int,node*>> backtrack,vector<int> v,vector<int> alpha,int min_sup);
+void FP_Growth_Merge(vector<set<int>> transactions, int min_sup, vector<int> alpha);
+
+void FP_Mine(vector<pair<int,node*>> backtrack,vector<int> v,vector<int> alpha,int min_sup)
+{
+    // cout <<"Here FPMINE\n";
+    unordered_map <int,vector<node *>> mp;
+    reverse(v.begin(),v.end());
+    for(auto i:backtrack)
+    {
+        mp[i.first].push_back(i.second);
+    }
+    // for(auto i: mp)
+    // {
+    //     //cout<<i.first<<" -> ";
+    //     for(auto j : i.second)
+    //         //cout<<j<<" ";
+    //     //cout <<endl;
+    // }
+    //cout <<endl;
+    for(auto i:v)
+    {
+        vector<set<int>> transaction;
+        for(auto j : mp[i])
+        {
+            // Put in the new transaction array here.
+            node *cur = j;
+            set<int> tra;
+            while(cur->parent->item != -1)
+            {
+                tra.insert(cur->parent->item);
+                cur = cur->parent;
+            }
+            for(int i = 0; i< j->freq; i++)
+                transaction.push_back(tra);
+            // rememer to push the answers in the vector
+        }
+        vector<int> beta;
+        //cout <<"ALPHA : ";
+        for(auto j : alpha)
+        {
+            beta.push_back(j);
+            //cout <<j<<" ";
+        }
+        beta.push_back(i);
+        //cout <<i<<" "<<endl;
+        finalans.push_back(beta);
+        FP_Growth(transaction,min_sup,beta);
+        // break;
+        // Call the make tree function here
+    }
+}
+
+void FP_Growth( vector< set<int> > transactions, int min_sup, vector<int> alpha )
+{
+    // cout <<"Here\n";
+    vector<pair<int,node*>> backtrack;
+    unordered_map<int,int> m0;
+    // int hash[1000000] = {0};
+    vector<int> v;
+    //SORTING THE TRANSACTIONS BASED ON THE OCCURENCE OF EACH ITEM
+        for(auto i : transactions)
+        {
+            for(auto j : i)
+            {
+                m0[j]++;
+                // hash[j]++;
+            }
+        }
+        set<int> dellist;
+        for(auto i : m0)
+        {
+            if(i.second >= min_sup)
+                v.push_back(i.first);
+            else
+            {
+                dellist.insert(i.first);
+            } 
+        }
+        if(v.empty())
+        {
+            return;
+        }
+        sort(v.begin(),v.end(),[m0](int i1,int i2) -> bool {  
+        if(m0.at(i1) == m0.at(i2))
+            return i1 < i2;  
+        return m0.at(i1) > m0.at(i2); 
+        });
+        vector<vector<int>> transac_Gen;
+        for(int i = 0; i<transactions.size();i++)
+        {
+            vector<int> vt;
+            for(auto j:v)
+            {
+                if(transactions[i].find(j) != transactions[i].end())
+                    vt.push_back(j);
+            } 
+            transac_Gen.push_back(vt);
+        }
+    // FINISHED SORTING EACH TRANSACTION
+
+
+    // BUILD TREE
+        node *a = new node();
+        a->item = -1;
+        a->freq = 0;
+        a->parent = NULL;
+
+        for(auto i : transac_Gen)
+        {
+            node *cur = a;
+            for(auto j : i)
+            {
+                if(dellist.find(j) != dellist.end())
+                {
+                    break;
+                }
+                if(cur->pointer.find(j) != cur->pointer.end())
+                {
+                    cur->pointer[j]->freq++;
+                    cur = cur->pointer[j];
+                }
+                else
+                {
+                    node *temp = new node();
+                    temp->item = j;
+                    temp->freq = 1;
+                    temp->parent = cur;
+                    cur->pointer[j] = temp;
+                    cur = cur->pointer[j];
+                    backtrack.push_back({temp->item,temp});
+                }
+            }
+        }
+    // FINISH BUILD TREE
+
+
+    // CHECK FOR SINGLE PATH TREE AND PERFORM THE OPERATION FOR THAT ACCORDINGLY
+        node *cur = a;
+        int flag = 0;
+        while(cur != NULL)
+        {
+            if(cur->pointer.size() > 1)
+            {
+                flag = 1;
+                break;
+            }
+            else if(cur->pointer.size() == 0)
+            {
+                break;
+            }
+            else
+            {
+                for(auto j : cur->pointer)
+                {
+                    cur = j.second;
+                }
+            }  
+        }
+        if(flag == 0)
+        {
+            node *cur = a;
+            vector<int> gen;
+            while(1)
+            {
+                if(cur->pointer.size() == 0)
+                    break;
+                auto i = cur->pointer.begin();
+                gen.push_back(i->first);
+                cur = i->second;
+            }
+            int num = pow(2,gen.size());
+            for(int i = 1; i<num;i++)
+            {
+                string s = get_binary(i);
+                vector<int> ans_add;
+                for(auto i : alpha)
+                {
+                    ans_add.push_back(i);
+                }
+                for(int i = 0; i<gen.size();i++)
+                {
+                    if(s[i] == '1')
+                        ans_add.push_back(gen[i]);
+                }
+                finalans.push_back(ans_add);
+            }
+            node *ret = nullptr;
+            return;
+        }
+    // FINISH CHECK FOR SINGLE PATH TREE
+
+
+    // CALL FP_MINE IF NOT SINGLE PATH
+        // sort(backtrack.begin(),backtrack.end(),[m0](pair<int,node*> i1, pair<int,node*> i2)->bool {
+        //         if(m0.at(i1.first) == m0.at(i2.first))
+        //         {
+        //             return i1 > i2;
+        //         }
+        //         return m0.at(i1.first) < m0.at(i2.first); 
+        //         });
+        FP_Mine(backtrack,v,alpha,min_sup);
+    // FINISH CALL FP_MINE
+    
+    return;
+}
+
+void FP_Mine_Merge(vector<pair<int,node*>> backtrack,vector<int> v,vector<int> alpha,int min_sup)
+{
+    unordered_map <int,vector<node *>> mp;
+    reverse(v.begin(),v.end());
+    for(auto i:backtrack)
+    {
+        mp[i.first].push_back(i.second);
+    }
+    map<int,vector<set<int>>> final_transaction;
+    unordered_map<node*,int> visit;
+    for(auto i:backtrack)
+    {
+        visit[i.second] = 0;
+    }
+    for(auto ab:v)
+    {
+
+    for(auto bt:mp[ab])
+    {
+        if(visit[bt] == 1)
+            continue;
+        else
+        {
+            node *cur = bt;
+            vector<int> tra;
+            vector<node*> tra2;
+            while(cur->parent->item != -1)
+            {
+                tra.push_back(cur->parent->item);
+                tra2.push_back(cur->parent);
+                cur = cur->parent;
+            }
+            int size = tra.size();
+            node *temp = bt;
+            for(int i = 0; i<size;i++)
+            {
+                if(visit[temp] == 1)
+                    continue;
+                set<int> newtr;
+                for(int k = i; k<size;k++)
+                {
+                    newtr.insert(tra[k]);
+                }
+                for(int j = 0; j<temp->freq;j++)
+                {
+                    final_transaction[temp->item].push_back(newtr);
+                }
+                visit[temp] = 1;
+                temp = tra2[i];
+            }
+                
+        }
+    }
+    }
+    for(auto i:v)
+    {
+        vector<vector<int>> transaction;
+        // for(auto i : final_transaction[i])
+        // {
+        //     vector<int> tra;
+        //     for(auto j:i)
+        //     {
+        //         tra.push_back(j);
+        //     }
+        //     transaction.push_back(tra);
+        // }
+        vector<int> beta;
+        //cout <<"ALPHA : ";
+        for(auto j : alpha)
+        {
+            beta.push_back(j);
+            //cout <<j<<" ";
+        }
+        beta.push_back(i);
+        //cout <<i<<" "<<endl;
+        finalans.push_back(beta);
+        FP_Growth_Merge(final_transaction[i],min_sup,beta);
+        // break;
+        // Call the make tree function here
+    }
+}
+
+void FP_Growth_Merge(vector<set<int>> transactions, int min_sup, vector<int> alpha)
+{
+    // cout <<"Here\n";
+    vector<pair<int,node*>> backtrack;
+    unordered_map<int,int> m0;
+    // int hash[1000000] = {0};
+    vector<int> v;
+    //SORTING THE TRANSACTIONS BASED ON THE OCCURENCE OF EACH ITEM
+        for(auto i : transactions)
+        {
+            for(auto j : i)
+            {
+                m0[j]++;
+                // hash[j]++;
+            }
+        }
+        set<int> dellist;
+        for(auto i : m0)
+        {
+            if(i.second >= min_sup)
+                v.push_back(i.first);
+            else
+            {
+                dellist.insert(i.first);
+            } 
+        }
+        if(v.empty())
+        {
+            return;
+        }
+        sort(v.begin(),v.end(),[m0](int i1,int i2) -> bool {  
+        if(m0.at(i1) == m0.at(i2))
+            return i1 < i2;  
+        return m0.at(i1) > m0.at(i2); 
+        });
+        vector<vector<int>> transac_Gen;
+        for(int i = 0; i<transactions.size();i++)
+        {
+            vector<int> vt;
+            for(auto j:v)
+            {
+                if(transactions[i].find(j) != transactions[i].end())
+                    vt.push_back(j);
+            } 
+            transac_Gen.push_back(vt);
+        }
+    // FINISHED SORTING EACH TRANSACTION
+
+
+    // BUILD TREE
+        node *a = new node();
+        a->item = -1;
+        a->freq = 0;
+        a->parent = NULL;
+
+        for(auto i : transac_Gen)
+        {
+            node *cur = a;
+            for(auto j : i)
+            {
+                if(dellist.find(j) != dellist.end())
+                {
+                    break;
+                }
+                if(cur->pointer.find(j) != cur->pointer.end())
+                {
+                    cur->pointer[j]->freq++;
+                    cur = cur->pointer[j];
+                }
+                else
+                {
+                    node *temp = new node();
+                    temp->item = j;
+                    temp->freq = 1;
+                    temp->parent = cur;
+                    cur->pointer[j] = temp;
+                    cur = cur->pointer[j];
+                    backtrack.push_back({temp->item,temp});
+                }
+            }
+        }
+    // FINISH BUILD TREE
+
+
+    // CHECK FOR SINGLE PATH TREE AND PERFORM THE OPERATION FOR THAT ACCORDINGLY
+        node *cur = a;
+        int flag = 0;
+        while(cur != NULL)
+        {
+            if(cur->pointer.size() > 1)
+            {
+                flag = 1;
+                break;
+            }
+            else if(cur->pointer.size() == 0)
+            {
+                break;
+            }
+            else
+            {
+                for(auto j : cur->pointer)
+                {
+                    cur = j.second;
+                }
+            }  
+        }
+        if(flag == 0)
+        {
+            node *cur = a;
+            vector<int> gen;
+            while(1)
+            {
+                if(cur->pointer.size() == 0)
+                    break;
+                auto i = cur->pointer.begin();
+                gen.push_back(i->first);
+                cur = i->second;
+            }
+            int num = pow(2,gen.size());
+            for(int i = 1; i<num;i++)
+            {
+                string s = get_binary(i);
+                vector<int> ans_add;
+                for(auto i : alpha)
+                {
+                    ans_add.push_back(i);
+                }
+                for(int i = 0; i<gen.size();i++)
+                {
+                    if(s[i] == '1')
+                        ans_add.push_back(gen[i]);
+                }
+                finalans.push_back(ans_add);
+            }
+            node *ret = nullptr;
+            return;
+        }
+    // FINISH CHECK FOR SINGLE PATH TREE
+
+
+    // CALL FP_MINE IF NOT SINGLE PATH
+        // sort(backtrack.begin(),backtrack.end(),[m0](pair<int,node*> i1, pair<int,node*> i2)->bool {
+        //         if(m0.at(i1.first) == m0.at(i2.first))
+        //         {
+        //             return i1 > i2;
+        //         }
+        //         return m0.at(i1.first) < m0.at(i2.first); 
+        //         });
+        FP_Mine(backtrack,v,alpha,min_sup);
+    // FINISH CALL FP_MINE
+    
+    return;
+}
+
+void show_results( vector <vector <int> >  answer)
+{
+	vector <vector <vector <int> > > ans;
+    int si=1;
+    vector < vector <int> > temp;
+    for(auto it:finalans)
+    {
+    	if(it.size()==si)
+    	{
+    		temp.push_back(it);
+    	}
+    	else
+    	{
+    		ans.push_back(temp);
+    		temp.clear();
+    		si++;
+    		temp.push_back(it);
+    	}
+    }
+    if(temp.size()!=0)
+    {
+    	ans.push_back(temp);
+    }
+	int z=0,j,k;
+	if(ans.size()!=0)
+    for(auto i : ans)
+    {
+    	cout << "\nNUMBER OF FREQUENT ITEMSETS OF SIZE=" << ++z << " ARE " << i.size() << " WHICH ARE-\n\n[";
+        for(j=0;j<i.size()-1;j++)
+        {
+        	cout << "[";
+        	for(k=0;k<i[j].size()-1;k++)
+        	{
+        		cout << i[j][k] << ",";
+        	}
+        	cout << i[j][k] << "],";
+        }
+        cout << "[";
+        for(k=0;k<i[j].size()-1;k++)
+        {
+        	cout << i[j][k] << ",";
+        }
+        cout << i[j][k] << "]";
+        cout << "]\n";
+    }
+    else
+    {
+    	cout << "\nNO FREQUENT ITEMSETS\n";
+    }
+    cout << "\n\n";
+}
+
+signed main()
+{
+    // step 1 - use Curl to read the file eg. curl 'http://www.philippe-fournier-viger.com/spmf/datasets/BMS1_spmf' > abc.txt 
+    string file_path,line;
+	cout << "Input The Path To The File -> ";
+	cin >> file_path;
+	ifstream fin(file_path, ios::in);
+    int i = 0;
+    vector<set<int>> v;
+    int abc = 0;
+    while(getline(fin, line))
+    {
+        // //cout<<line<<endl;
+        stringstream s(line);
+        string word;
+        set<int> t;
+        abc++;
+        while(getline(s,word,' '))
+        {
+            int temp = stoi(word);
+            if(temp > 0)
+            {
+                t.insert(temp);
+            }
+        }
+        v.push_back(t);
+    }
+    double min_sup,n;
+    cout << "Input The Minimum Support Count (in percentage) -> ";
+    cin >> min_sup;
+    n=(double)v.size();
+    n*=min_sup;
+    n/=100;
+    int minsup=n;
+    cout << "Minimum Support Count Is " << minsup << "\n";
+    vector<int> alpha;
+    auto start = high_resolution_clock::now();
+    FP_Growth(v,minsup,alpha);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start); 
+    cout << "\nTIME TAKEN BY ALGORITHM WITHOUT OPTIMISATION "<< float(duration.count()/1000000.0) << " seconds" << endl;
+
+    sort(finalans.begin(),finalans.end(),[](vector<int> v1,vector<int>v2)->bool
+    {
+        return v1.size() < v2.size();
+    });
+    show_results(finalans);
+
+    finalans.clear();
+    alpha.clear();
+    start = high_resolution_clock::now();
+    FP_Growth_Merge(v,minsup,alpha);
+    stop = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(stop - start); 
+    cout << "\nTIME TAKEN BY ALGORITHM WITH MERGE OPTIMISATION "<< float(duration.count()/1000000.0) << " seconds" << endl;
+    sort(finalans.begin(),finalans.end(),[](vector<int> v1,vector<int>v2)->bool
+    {
+        return v1.size() < v2.size();
+    });
+    show_results(finalans);
+	return 0;
+}
